ef3b44d58bf535255ac5413f53be8325b7ed4e10
Kirat <mourad.kirat.9@gmail.com>
2024-02-23 08:34:09 +0100

    Initial Commit

---
+++ CoreBluetoothExample/CoreBluetoothExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothExampleApp.swift
+//  CoreBluetoothExample
+//
+//  Created by mac on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
