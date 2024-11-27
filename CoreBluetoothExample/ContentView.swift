5068b2e81daa039324803523459e1f571f70cb92
Mourad KIRAT <mourad.kirat.9@gmail.com>
2024-11-27 10:59:49 +0100

    SwiftUI- Core Bluetooth

--- CoreBluetoothExample/ContentView.swift
+++ CoreBluetoothExample/ContentView.swift
@@ -2,20 +2,73 @@
 //  ContentView.swift
 //  CoreBluetoothExample
 //
-//  Created by mac on 23/02/2024.
+//  Created by Mourad KIRAT  on 23/02/2024.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @StateObject var bluetoothManager = BluetoothManager()
+    @State private var color:String = "242424"
+    @State private var statebluetooth:Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack {
+                Image("image_title")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250.0, height:75.0,alignment: .leading)
+              
+            }.background(Color.init(UIColor(hexString: self.color)))
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(10)
+            // Form 1
+Form {
+    //section 1
+     Section(header:Text("State bluetooth")
+            .foregroundColor(Color.white)
+             
+            .font(.system(size: 20, weight: .bold))){
+                Toggle(isOn: $statebluetooth) {
+                    Text(bluetoothManager.isBluetoothEnabled ? "Turn Off Bluetooth" : "Turn On Bluetooth")
+                        .font(.system(size: 17, weight: .medium))
+                        .foregroundColor(Color.init(UIColor(hexString: "#000000")))
+                    }.tint(Color.init(UIColor(hexString: "#00BFFF"))) // End
+                     .frame(maxWidth: .infinity, alignment: .leading)
+                     .onChange(of:statebluetooth) { value in
+                         bluetoothManager.toggleBluetooth()
+                        // self.statebluetooth = bluetoothManager.isBluetoothEnabled
+                                            }
+                            
+     } .listRowBackground(Color.white)
+    // Section 2
+    if  self.statebluetooth  == true {
+                    List(bluetoothManager.discoveredPeripherals, id: \.identifier) { peripheral in
+                        Text(peripheral.name ?? "Unknown")
+                    }
+//    Section(header:Text("List peripheral names")
+//           .foregroundColor(Color.white)
+//           .font(.headline)){
+//               Text( "iPhone 13")
+//                   .font(.system(size: 17, weight: .medium))
+//               .foregroundColor(Color.init(UIColor(hexString: "#000000")))
+//               Text( "iPhone MKirat")
+//                   .font(.system(size: 17, weight: .medium))
+//               .foregroundColor(Color.init(UIColor(hexString: "#000000")))
+//                           
+//    } .listRowBackground(Color.white)
+    }
+                }.tint( Color.white)
+                    .background(Color.init(UIColor(hexString: self.color)))
+                    .scrollContentBackground(.hidden)
+                    .padding(10)
+            // End Form 1
+            
+            
+            
+        } // Vstack
+        .background(Color.init(UIColor(hexString: self.color)))
     }
 }
 
@@ -24,3 +77,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
---
+++ CoreBluetoothExample/Library/BleutoothManager.swift
@@ -0,0 +1,43 @@
+//
+//  BleutoothManager.swift
+//  CoreBluetoothExample
+//
+//  Created by Mourad KIRAT on 23/02/2024.
+//
+import CoreBluetooth
+
+class BluetoothManager: NSObject, CBCentralManagerDelegate, ObservableObject {
+    @Published var isBluetoothEnabled = false
+    @Published var discoveredPeripherals = [CBPeripheral]()
+
+    private var centralManager: CBCentralManager!
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            isBluetoothEnabled = true
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            isBluetoothEnabled = false
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !discoveredPeripherals.contains(peripheral) {
+            discoveredPeripherals.append(peripheral)
+        }
+    }
+
+    func toggleBluetooth() {
+        if centralManager.state == .poweredOn {
+            centralManager.stopScan()
+            centralManager = nil
+        } else {
+            centralManager = CBCentralManager(delegate: self, queue: nil)
+        }
+    }
+}
